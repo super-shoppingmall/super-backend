28566865d281560fdb91686a2a00a1115d0b74c4
chacha <89715044+chachapp@users.noreply.github.com>
2023-09-07 17:22:37 +0900

    Feat : paymoney 기능 개발(insert, update, select)

---
+++ src/main/java/com/github/superbackend/dto/PaymoneyRequest.java
@@ -0,0 +1,11 @@
+package com.github.superbackend.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+public class PaymoneyRequest {
+    private Integer paymoney;
+}
--- src/main/java/com/github/superbackend/repository/paymoney/PaymoneyRepository.java
+++ src/main/java/com/github/superbackend/repository/paymoney/PaymoneyRepository.java
@@ -1,8 +1,12 @@
 package com.github.superbackend.repository.paymoney;
 
+import com.github.superbackend.repository.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymoneyRepository extends JpaRepository<Paymoney, Long> {
+
+    Paymoney findAllByMember(Member member);
+    Integer findPaymoneyByMember(Member member);
 }
