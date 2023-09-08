4f670039b77b3bb2206fcbfc4670788a94dbaba3
jiyoung-B <catgirl0313@naver.com>
2023-09-08 20:15:54 +0900

    판매 물품 등록

--- src/main/java/com/github/superbackend/repository/product/Product.java
+++ src/main/java/com/github/superbackend/repository/product/Product.java
@@ -1,32 +1,51 @@
-//package com.github.superbackend.repository.product;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import java.time.LocalDateTime;
-//
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Entity
-//public class Product {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long productId;
-//    private String productName;
-//    private String categoryName;
-//    private String productPrice;
-//    private String productQuantity;
-//    private String productImages;
-//    private String productDetail;
-//    private String productOnSale;
-//    private LocalDateTime createdAt;
-//    private LocalDateTime updatedAt;
-//}
+package com.github.superbackend.repository.product;
+
+
+import com.github.superbackend.repository.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private String categoryName;
+    private int productPrice;
+    private int productQuantity;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductImage> images = new ArrayList<>();  //productImages;
+
+    private String productDetail;
+    //private String productOnSale;
+    private LocalDate closingAt; // 판매 종료
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;  // 등록한 멤버 (판매자 또는 구매자)
+
+    // @OneToMany(mappedBy="product", cascade=CascadeType.ALL)
+    // private List<Order> orders;  // 해당 상품에 대한 모든 주문들
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
+
