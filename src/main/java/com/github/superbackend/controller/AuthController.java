c8182c38d93ccd8b5b14533320523ec06e129f73
chacha <89715044+chachapp@users.noreply.github.com>
2023-09-09 12:11:26 +0900

    feat : EC2배포를 위한 deploy 브랜치, application.prod 추가

--- src/main/java/com/github/superbackend/controller/AuthController.java
+++ src/main/java/com/github/superbackend/controller/AuthController.java
@@ -47,7 +47,7 @@ public class AuthController {
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        String jwt = jwtUtil.generateToken(String.valueOf(authentication));
+        String jwt = jwtUtil.generateToken(loginRequest.getEmail()); //  loginRequest.getEmail()) String.valueOf(authentication)
 
         // 사용자 정보 추출 (예: 사용자 이름 및 권한)
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
