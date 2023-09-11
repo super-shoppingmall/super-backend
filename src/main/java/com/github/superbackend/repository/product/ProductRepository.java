b19251112d1078be447fa826771c4551dc341d96
jiyoung-B <catgirl0313@naver.com>
2023-09-12 02:03:47 +0900

    판매 물품 재고 수정, 판매 물품 내역 조회

--- src/main/java/com/github/superbackend/repository/product/ProductRepository.java
+++ src/main/java/com/github/superbackend/repository/product/ProductRepository.java
@@ -13,8 +13,14 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findByMemberAndClosingAtAfter(Member member, LocalDate now);
     @Modifying
     @Query("update Product p set p.productStatus = :status where p.closingAt < :now")
     int updateStatusForPastProducts(@Param("status") ProductStatus status, @Param("now") LocalDate now);
+
+    List<Product> findByMemberAndProductStatus(Member seller, ProductStatus status);
+
+    @Query("SELECT e FROM Product e WHERE e.productStatus = 'FINISHED' OR e.productStatus = 'CLOSED'")
+    List<Product> findByMemberAndProductStatusIsNotActive();
+
+    Product findByProductIdAndMember(Long productId, Member seller);
 }
