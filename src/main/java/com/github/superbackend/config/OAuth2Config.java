d362c7df415c7aca15de776735c002e878c7cd6e
han sanghoon <93518184+hasahooy@users.noreply.github.com>
2023-09-12 11:59:00 +0900

    Update OAuth2Config.java
    client id ,password 추가

--- src/main/java/com/github/superbackend/config/OAuth2Config.java
+++ src/main/java/com/github/superbackend/config/OAuth2Config.java
@@ -7,13 +7,15 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
@@ -27,36 +29,40 @@ public class OAuth2Config {
                 kakaoClientRegistration()
         );
     }
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
+    }
 
     private ClientRegistration naverClientRegistration() {
         return ClientRegistration.withRegistrationId("naver")
-                .clientId("YOUR_NAVER_CLIENT_ID")
-                .clientSecret("YOUR_NAVER_CLIENT_SECRET")
+                .clientId("AN7ep_CD_ii1ftJlANpq")
+                .clientSecret("BxmrzjFAhZ")
                 .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUriTemplate("{baseUrl}/login/oauth2/code/naver")
-                .scope("profile")
+                .scope("name", "email", "profile_image")
                 .authorizationUri("https://nid.naver.com/oauth2.0/authorize")
                 .tokenUri("https://nid.naver.com/oauth2.0/token")
                 .userInfoUri("https://openapi.naver.com/v1/nid/me")
-                .userNameAttributeName("id")
-                .clientName("Naver")
+                .userNameAttributeName("response")
+                .clientName("supercoding")
                 .build();
     }
 
     private ClientRegistration kakaoClientRegistration() {
         return ClientRegistration.withRegistrationId("kakao")
-                .clientId("YOUR_KAKAO_CLIENT_ID")
-                .clientSecret("YOUR_KAKAO_CLIENT_SECRET")
+                .clientId("acc3bcadd86193c806f6391bcad98507")
+                .clientSecret("tPH1IS0ZGwCxuMPUkr6OCl0ywlFSLltx")
                 .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUriTemplate("{baseUrl}/login/oauth2/code/kakao")
-                .scope("profile")
+                .scope("profile_nickname", "account_email")
                 .authorizationUri("https://kauth.kakao.com/oauth/authorize")
                 .tokenUri("https://kauth.kakao.com/oauth/token")
                 .userInfoUri("https://kapi.kakao.com/v2/user/me")
-                .userNameAttributeName("id")
-                .clientName("Kakao")
+                .userNameAttributeName("response")
+                .clientName("supercoding")
                 .build();
     }
 }
